39a02ac1ca362c552bbf89c0e534d7b3d3ff31b2
Christian Iversen <thebigecko@gmail.com>
2022-07-25 13:39:16 +0200

    Page setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+import { Home } from './pages/Home';
 function App() {
-  return <div>Developer academy!</div>;
+  return (
+    <div>
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
