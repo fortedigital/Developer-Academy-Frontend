f50f6678a194915d087bf86143ff50c67dbcf4c7
Christian Iversen <thebigecko@gmail.com>
2022-07-25 15:29:24 +0200

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,23 @@ import { Footer } from './components/Footer';
 import { Header } from './components/Header';
 import { Order } from './pages/Order';
 import { IShoppingCart } from './interfaces/shoppingCart';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { NotFound } from './pages/NotFound';
 
 const ShoppingCartContext = React.createContext<IShoppingCart>([]);
 
 function App() {
   const [cart, setCart] = useState<IShoppingCart[]>([]);
   return (
-    <>
-      {/*TODO: Add React Router */}
-      <ShoppingCartContext.Provider value={[cart, setCart]}>
-        <Header />
-        <Order />
-        <Footer />
-      </ShoppingCartContext.Provider>
-    </>
+    <ShoppingCartContext.Provider value={[cart, setCart]}>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Order />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ShoppingCartContext.Provider>
   );
 }
 
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export const NotFound = () => {
+  return (
+    <div>
+      <h2>404</h2>
+      <p>Page not found</p>
+    </div>
+  );
+};
