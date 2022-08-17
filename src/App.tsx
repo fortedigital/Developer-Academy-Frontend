6ad86b1c039392975582dc8822b6d20c0112187a
Eric Veliyulin <eric.vel@outlook.com>
2022-08-17 16:00:42 +0200

    Remove ShoppingCartContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import NewOrder from "./pages/NewOrder";
 import MyOrders from "./pages/MyOrders";
 import NotFound from "./pages/NotFound";
 import Layout from "./components/Layout";
-import { IShoppingCart } from "./interfaces/shoppingCart";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-export const ShoppingCartContext = React.createContext<IShoppingCart>([]);
-
 function App() {
-  const [cart, setCart] = useState<IShoppingCart>({} as IShoppingCart);
   return (
-    <ShoppingCartContext.Provider value={[cart, setCart]}>
-      <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<NewOrder />} />
-            <Route path="/mine-bestillinger" element={<MyOrders />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Layout>
-      </BrowserRouter>
-    </ShoppingCartContext.Provider>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<NewOrder />} />
+          <Route path="/mine-bestillinger" element={<MyOrders />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
--- src/interfaces/shoppingCart.ts
+++
@@ -1,3 +0,0 @@
-export interface IShoppingCart {
-  //TODO: Implement interface
-}
