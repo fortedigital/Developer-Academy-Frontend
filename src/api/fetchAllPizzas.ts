86da24b3176eb5970a4e6b7ef12ca2352f3d8c3e
Eric Veliyulin <eric.vel@outlook.com>
2022-08-23 22:54:31 +0200

    Setup checkpoint 2

--- src/api/fetchAllPizzas.ts
+++ src/api/fetchAllPizzas.ts
@@ -1,14 +1,7 @@
-import { IPizza } from '../interfaces/pizza';
+import { dummyPizzas } from "../data/dummyPizzas";
+import { IPizza } from "../interfaces/pizza";
 
 export const fetchAllPizzas = async (): Promise<IPizza[]> => {
   // TODO: Hent og returner pizzaer fra API istedenfor fra dummy-data
-  return fetch('https://kjetilpizzaapi.azurewebsites.net/api/pizza')
-    .then((res) => res.json())
-    .then((data) => {
-      return data;
-    })
-    .catch((err) => {
-      console.error(err);
-      return [];
-    });
+  return dummyPizzas;
 };
